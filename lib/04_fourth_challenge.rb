cb5d1ab27480ec87dc7ad4546cf877c826adffac
casual-karma-e108 <akravecas@yahoo.com>
2021-02-19 16:58:38 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -5,7 +5,7 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        :Mercutio =>{:name =>"Benvolio",:age "17"=>, :attitude => "worried" },
+        :Benvolio =>{:name =>"Benvolio",:age "17"=>, :attitude => "worried" },
         :Mercutio => {:name =>"Mercutio",:age "18"=>, :attitude => "hot-headed" }}
       ]
    }, 
@@ -14,8 +14,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        :Benvolio =>{:name =>"Benvolio",:age "17"=>, :attitude => "worried" },
-        :Mercutio =>{:name =>"Mercutio",:age "18"=>, :attitude => "hot-headed" }}
+        :Steven =>{:name =>"Steven",:age "30"=>, :attitude => "confused" },
+        :Nurse =>{:name =>"Nurse",:age "44"=>, :attitude => "worried"}}
       ]
    }
 }
